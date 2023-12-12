34474e3f5e56f8854d6e3162be30b7087c54aedc
“muddyrain” <qiu3291002845@gmail.com>
2023-12-12 18:39:48 +0800

    feat: loading

--- app/dashboard/settings/page.js
+++ app/dashboard/settings/page.js
@@ -1,4 +1,11 @@
-// app/dashboard/settings/page.js
-export default function Page() {
-  return <h1>Hello, Settings!</h1>;
+// app/dashboard/page.js
+async function getData() {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  return {
+    message: 'Hello, Dashboard!',
+  };
+}
+export default async function DashboardPage(props) {
+  const { message } = await getData();
+  return <h1>{message}</h1>;
 }
