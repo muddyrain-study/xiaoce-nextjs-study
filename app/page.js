3eeb5dd3cfe854247a09c3075d384404c344e97c
“muddyrain” <qiu3291002845@gmail.com>
2023-12-13 22:37:50 +0800

    feat: error 错误处理

--- app/page.js
+++ app/page.js
@@ -1,37 +1,15 @@
 'use client';
-import React, { Profiler, Suspense } from 'react';
-import AsyncComponent from '../src/AsyncComponent';
+// dashboard/page.js
+import React from 'react';
 
-// 异步加载数据的函数
-const fetchData = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('Data loaded');
-    }, 2000);
-  });
-};
-function App() {
-  const onRender = (
-    id,
-    phase,
-    actualDuration,
-    baseDuration,
-    startTime,
-    commitTime
-  ) => {
-    console.log(id, phase, actualDuration, baseDuration, startTime, commitTime);
+export default function Page() {
+  const [error, setError] = React.useState(false);
+
+  const handleGetError = () => {
+    setError(true);
   };
-  console.log('App-render');
+
   return (
-    <div>
-      <h1>My App</h1>
-      <Profiler id='App' onRender={onRender}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <AsyncComponent />
-        </Suspense>
-      </Profiler>
-    </div>
+    <>{error ? Error() : <button onClick={handleGetError}>Get Error</button>}</>
   );
 }
-
-export default App;
